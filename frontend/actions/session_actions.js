efc183f2bc47918b72cbcf310dc007a303ce0084
Josh Choi <joshuachoi0508@gmail.com>
2018-12-10 09:51:02 -0800

    Create actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,27 +1,50 @@
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERROS = 'RECEIVE_SESSION_ERROS';
 
 //receive current users
 
+
 //object
-export const receiveCurrentUser = user => ({
+export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
-  user
+  currentUser
 });
 //thunk
-export const signUp = user => (
+export const signUp = user => dispatch => (
   APIUtil.signup(user)
-    .then(receivedUser => receiveCurrentUser(receivedUser))
-)
+    .then(receivedUser => dispatch(receiveCurrentUser(receivedUser)),
+          err => (dispatch(receiveErrors(err.responseJSON)))
+));
+
+export const login = currentUser => dispatch => (
+  APIUtil.login(currentUser)
+    .then(currentUser => dispatch(receiveCurrentUser(currentUser)),
+          err => dispatch(receiveErrors(err.responseJSON)))
+);
 
 //logout current users
 
 
 //object
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
 
 //thunk
+export const logout = () => dispatch => (
+  APIUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+);
 
 //receive errors
 
 //object
+export const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERROS,
+  errors
+});
 
 //thunk

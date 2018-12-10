197c7159ac55f4a4357c372885de968f789749e6
Josh Choi <joshuachoi0508@gmail.com>
2018-12-10 10:04:39 -0800

    Create users reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const usersReducer = (oldState = {}, action) => {
+  switch(action.type){
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, oldState, { [action.currentUser.id]: action.currentUser })
+    default:
+      return oldState;
+  }
+};
+
+export default usersReducers;
